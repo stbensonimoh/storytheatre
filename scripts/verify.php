37a8e6fd35666e21cf0630550a4dcb68d892f2db
Benson Imoh, ST <benson@stbensonimoh.com>
2019-03-30 15:08:43 +0100

    remove error display

--- scripts/verify.php
+++ scripts/verify.php
@@ -10,8 +10,8 @@
  * @license  MIT https://opensource.org/licenses/MIT
  * @link     https://stbensonimoh.com
  */
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 require '../config.php';
 require './Paystack.php';
 require './DB.php';
